f7873abd9313e7342697a07d29a2ba150593da91
Chris Burgess <cgburgess10@gmail.com>
2023-07-10 11:35:39 -0400

    todos

--- init.lua
+++ init.lua
@@ -1,2 +1,9 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+
+-- TODO:
+-- Better terminals
+-- custom telescope and lsp config
+-- better colors
+-- copilot statusline symbol
