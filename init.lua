9add55f6cf4617f755e472d310d17932ddb23ed3
Chris Burgess <cgburgess10@gmail.com>
2023-07-12 22:48:37 -0400

    add note about autocommand

--- init.lua
+++ init.lua
@@ -1,9 +1,13 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
-
 -- TODO:
 -- Better terminals
 -- custom telescope and lsp config
 -- better colors
 -- copilot statusline symbol
+
+-- Nice auto commands to add
+
+-- Run Zig Build on save of zig files
+-- augroup zigbuild | exe "au BufWritePost *.zig TermExec cmd='zig build'" | augroup end
