f0c3594c9bfe2f61b4bff4d80a2e0d4104770b36
Chris Burgess <cgburgess10@gmail.com>
2023-07-22 20:26:44 -0400

    Update copilot

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -7,28 +7,31 @@ local function accept_word()
 end
 
 return {
-  "github/copilot.vim",
-  lazy = true,
-  -- enable when entering insert mode
-  event = "InsertEnter",
-  config = function()
-    vim.g.copilot_filetypes = {
-      markdown = true,
+  {
+    "github/copilot.vim",
+    lazy = true,
+    -- enable when entering insert mode
+    event = "InsertEnter",
+    config = function()
+      vim.g.copilot_filetypes = {
+        markdown = true,
+        gitcommit = true,
+      }
+    end,
+    keys = {
+      {
+        "<leader>cp",
+        "<cmd>Copilot<cr>",
+        desc = "Copilot suggestions",
+      },
+      {
+        mode = "i",
+        "<C-l>",
+        accept_word,
+        desc = "Accept Copilot suggestion",
+        expr = true,
+        remap = false,
+      }
     }
-  end,
-  keys = {
-    {
-      "<leader>cp",
-      "<cmd>Copilot<cr>",
-      desc = "Copilot suggestions",
-    },
-    {
-      mode = "i",
-      "<C-l>",
-      accept_word,
-      desc = "Accept Copilot suggestion",
-      expr = true,
-      remap = false,
-    }
-  }
+  },
 }
