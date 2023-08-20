e6aad3b4742d1c5adbf53f19c2c89eb699cb1477
Chris Burgess <cgburgess10@gmail.com>
2023-08-20 10:31:19 -0400

    Add tokyo night

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -29,6 +29,12 @@ return {
   },
   { 'projekt0n/github-nvim-theme' },
   {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "github_dark_high_contrast",
